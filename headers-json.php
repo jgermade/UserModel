f13398497616c4d50592e775e2f29583d7eac62b
Jesus Germade <jesus@germade.es>
2014-04-17 15:01:11 +0000

    se anade headers-json.php

---
+++ headers-json.php
@@ -0,0 +1,7 @@
+<?php
+    ob_start ("ob_gzhandler");
+    header("Content-type: application/json");
+    header("charset: utf-8");
+    header("Cache-Control: no-cache");
+    header("Expires: -1");
+?>
