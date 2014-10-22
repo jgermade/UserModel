87f0fcef45c3832f923559fa1567be88a1176de6
Jesús Germade <jesus@germade.es>
2014-10-22 13:01:54 +0200

    Update UserModel.php

--- UserModel.php
+++ UserModel.php
@@ -553,7 +553,7 @@
     
     
     class ModelSQL {
-        private $host = DB_HOST || "localhost";
+        private $host = isset(DB_HOST) ? DB_HOST : "localhost";
         private $user = DB_USER;
         private $pass = DB_PASS;
         private $db_name = DB_NAME;
